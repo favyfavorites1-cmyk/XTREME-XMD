c1e3c78e9b1aafcd7e2213ae7952e8b122991569
favyfavorites1-cmyk <favyfavorites1@gmail.com>
2025-08-11 19:45:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~X9RHAZDS#2p4wPmegdHb4WmJiUGEjC3T_1oNVu2UTTLKnorgDLAA",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~vsIUjZJB#OGeEpbA7dchE2QCE27kxYiLReiGzGmoa66oEzaj4ZwM",
 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
@@ -25,7 +25,7 @@ FAKE_TYPING: process.env.FAKE_TYPING || "false",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
-HEART_REACT: process.env.HEART_REACT || "false",
+HEART_REACT: process.env.HEART_REACT || "true",
 OWNER_REACT: process.env.OWNER_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "𝗫𝗧𝗥𝗘𝗠𝗘-𝗫𝗠𝗗",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
